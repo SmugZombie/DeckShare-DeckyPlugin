4833845ca2e090f6b1d60a4849e7f989ddb22b3c
Ron Egli <ron@egli.me>
2024-03-18 19:36:38 -0700

    updates

--- src/state/ScreenshotsState.tsx
+++ src/state/ScreenshotsState.tsx
@@ -24,7 +24,7 @@ interface PublicScreenshotsContext extends PublicScreenshotsState {
 }
 
 interface PublicUploadQueueContext extends PublicScreenshotsState {
-  setUploadQueue(screenshots: ScreenshotsDictionary): void;
+  setUploadQueue(uploadQueue: ScreenshotsDictionary): void;
 }
 
 export class ScreenshotsState {
